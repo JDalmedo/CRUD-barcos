63a1c1a67ac77f050ca35d3109403a22aa469df4
UsuarioM <jjdalmedo@gmail.com>
2023-11-02 14:30:38 +0100

    3

--- index.js
+++ index.js
@@ -8,30 +8,58 @@ const connect = require("./utils/db");
 const Barco = require("./models/barco.model");
 
 const server = express();
-
+connect();
 server.use(express.json());
 server.use(express.urlencoded({extended: false}));
-connect();
 
 const router = express.Router();
-
-router.post("/create", async (req, res) => {
-    try {
-        const newBarco = new Barco(req, body);
-        console.log(newBarco);
-        await newBarco.save();
-        return res.status(201).json(newBarco);
-    }   catch(error){
-        return res.status(400).json("Error creating Barco");
-    }    
-});
 
 router.get("/barcos", async (req, res) => {
     try {
         const barcos = await Barco.find();
         return res.status(200).json(barcos);
     } catch (error) {
-        return res.status(404).json("Barcos not found");
+        return res.status(400).json("Barcos not found");
+      }
+});
+router.get("barcos/:id", async (req, res) => {
+    try{
+        const { id } = req.params;
+        const barco = await Barco.findById(id);
+        return res.status(200).json(barco);
+      } catch (error) {
+        return res.status(404).json("Barco not found");
+      } 
+    });
+
+router.post("/barcos", async (req, res) => {
+    try {
+        const newBarco = new Barco(req.body);
+        await newBarco.save();
+        return res.status(201).json(newBarco);
+    }   catch(error){
+        return res.status(500).json("Error creating Barco");
+    }    
+});
+
+router.delete("/barcos/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        const barco = await Barco.findByIdAndDelete(id);
+        return res.status(200).json(barco);
+      } catch(error){
+        return res.status(500).json("Error deleting Barco");
+      }
+
+});
+
+router.patch("/barcos/:id", async (req, res) => {
+    try {
+        const {id} = req.params;
+        const barco = await Barco.findByIdAndUpdate(id);
+        return res.status(200).json(barco);
+      } catch(error){
+        return res.status(500).json("Error updating Barco");
       }
 });
 
@@ -40,5 +68,5 @@ server.use("/", router);
 const PORT = process.env.PORT;
 
 server.listen(PORT, () => {
-    console.log(`Server runnig on http://localhost:${PORT}`)
+    console.log(`Server runnig on http://localhost:${PORT}`);
 });
--- models/barco.model.js
+++ models/barco.model.js
@@ -1,13 +1,8 @@
 const mongoose = require("mongoose");
 
 const barcoSchema = new mongoose.Schema({
-    category: {type: String, enum: ["Military", "Passengers", "Cargo"], required: true},
-    name: {type: String, required: true},
-    IMO: {type: Number, required: true},
-    tipology: {type: String, required: true},
-    cover: {type: String, required: true},
-    launching: {type: Number, default: true},
-    shipyard: {type: String, required:true},
+    category: {type: String, required: true}
+   
 },
 
 {
