049df1dbeaf86ee8e7156771d961dca2f9013441
JDalmedo <jjdalmedo@gmail.com>
2023-11-05 08:47:59 +0100

    db

--- models/barco.model.js
+++ models/barco.model.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
 
 const barcoSchema = new mongoose.Schema({
-    category: {type: String, required: true}
-   
-},
+    category: {type: String, required: true, trim: true},
+    name: {type: String, required: true, trim: true},
+    IMO: {type: Number, required: true},
+    tipology: {type: String, required: true, trim: true},
+    cover: {type: String, required: true},
+    flag: {type: String, required: true, trim: true},
+    shipyard: {type: String, required: true, trim: true},
+    launching: {type: Number, required: true}
+ },
 
-{
+ {
     timestamps: true,
-}
+ }
 );
 
 const barco = mongoose.model("barco", barcoSchema);
